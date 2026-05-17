21273c20bf6417529eb227bf5ce790d2fc5b53ca
Aslanli Aslan <noiocaaa@gmail.com>
2026-05-17 19:03:41 +0400

    fix: correct the implementation of root directory

--- src/jobanalysis/paths.py
+++ src/jobanalysis/paths.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-ROOT = Path.cwd()
+ROOT = Path(__file__).parent.parent.parent
 
 RAW_DATA = ROOT / "data/raw/vacancies.db"
 PROCESSED_DATA = ROOT / "data/processed/vacancies.db"
